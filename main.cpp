e8f596885c5888992fea2e024337761d32d32291
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-04-16 11:44:08 +0200

    main.cpp creation

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<cstdlib>
+#include<sstream>
+#include<limits>
+#include<string>
+
+#include "output.h"
+
+using namespace std;
+
+
+int main()
+{
+
+}
