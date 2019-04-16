d2600efafe281826c9d5be78a652bfe153367949
HaasL <49677732+HaasL@users.noreply.github.com>
2019-04-16 14:09:29 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
-#include<fstream>
-#include<ctime>
-#include<cstdlib>
-#include<sstream>
-#include<limits>
-#include<string>
+//
+//  main.cpp
+//  C++ Paris 1
+//
+//  Created by Lillian Haas on 21/03/2019.
+//  Copyright © 2019 Lillian Haas. All rights reserved.
+//
 
-#include "output.h"
+#include <iostream>
 
-using namespace std;
-
-
-int main()
-{
-
+using namespace std; 
+int main() {
+    
+    int a,b,c;
+    
+    cout<<"Number 1";
+    cin>>a;
+    cout<<"Number 2";
+    cin>>b;
+    cout<<"Number 3";
+    cin>>c;
+    
+    if (a>b)
+    {
+        if (a<c) {
+            cout<<"greatest number"<<a<<endl;
+        } else {
+            cout<<"greatest number"<<c<<endl;
+        }
+    } else {
+        cout<<"greatest number"<<b<<endl;
+    }
 }
