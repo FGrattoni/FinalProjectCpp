f82d3e746d997de85bb5ec62906a7b2ccfe1433a
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-04-17 12:42:58 +0200

    moved frequency() in frequency.h and included it

--- main.cpp
+++ main.cpp
@@ -3,46 +3,16 @@
 #include <cmath>
 
 #include "MissingElement.h"
+#include "frequency.h"
 
 /*
 Final project: Loan Payment Schedule
 Date 16th April 2019
 Names Giada, Fabio, Lillian 
- ///**/
+*/
 
 
 using namespace std;
-
-// General Set-up Environment
-
-//Create Rate of Repayment
-
-void Frequency(int i=0) {
-    double f;
-    if (i==0){
-    cout << "Hello," << endl;
-    cout
-            << "Considering your repayment schedule, how often would you like install repayment rates install payments per year?\n";
-    cout << "Available rate options are:\n";
-    }
-    cout << " 1 = annually \n 2 = semi-annually \n 3 = quarterly \n 4 = monthly\n";
-    cout << "Insert your choice by the provided numbers: \n";
-    cin >> f;
-
-    if (f==1){
-    cout<<"You selected annual repayment rate\n";
-    }else if(f==2){
-        cout<<"You selected semi-annual repayment rates\n";
-    }else if(f==3){
-        cout<<"You selected quarterly repayment rates\n";
-    }else if(f==4){
-        cout<<"You selected monthly repayment rates\n";
-    }else {
-        cout<<"Please insert a valid input.\n\n";
-        Frequency(i=1);
-    }
-}
-
 
 int main()
 {
