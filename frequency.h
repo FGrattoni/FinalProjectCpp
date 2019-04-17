97d2efa4ad2e2f14b072e0f9ed8b1a599bb0437c
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-04-17 12:41:41 +0200

    Moved frequency() here

---
+++ frequency.h
@@ -0,0 +1,38 @@
+#ifndef frequency_H
+#define frequency_H
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int Frequency(int i=0) {
+    double f;
+    if (i==0){
+    cout << "Hello," << endl;
+    cout
+            << "Considering your repayment schedule, how often would you like install repayment rates install payments per year?\n";
+    cout << "Available rate options are:\n";
+    }
+    cout << " 1 = annually \n 2 = semi-annually \n 3 = quarterly \n 4 = monthly\n";
+    cout << "Insert your choice by the provided numbers: \n";
+    cin >> f;
+
+    if (f==1){
+    cout<<"You selected annual repayment rate\n";
+    }else if(f==2){
+        cout<<"You selected semi-annual repayment rates\n";
+    }else if(f==3){
+        cout<<"You selected quarterly repayment rates\n";
+    }else if(f==4){
+        cout<<"You selected monthly repayment rates\n";
+    }else {
+        cout<<"Please insert a valid input.\n\n";
+        Frequency(i=1);
+    }
+    
+    return f;
+}
+
+
+#endif
