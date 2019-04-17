5b9ba2ec85b1c2f7d1438cc66b1645f9ab2c2926
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-04-17 12:35:37 +0200

    Modified and added all formulas
    Added all formulas to compute the missing values.
    Modified some text.
    
    All of the functions are now just called in the main.cpp file by MissingElementFI()
    
    NOTE:
    - formula for n is wrong.
    - need to fix the case in which a string is inserted

--- MissingElementFI.h
+++ MissingElementFI.h
@@ -1,76 +1,134 @@
-#include <iostream>
-#include <cmath>
+#ifndef MissingElementFI_H
+#define MissingElementFI_H
+
+#include<iostream>
+#include<cmath>
 
 using namespace std;
 
-void MissingElementFI () {
+// CASE 1: PV computation
+double PVComputation(double PMT, int n, double r)
+{
+	double PV;
+	double temp;
+	temp = pow(1+r,n);
+	PV = PMT * ((temp-1)/(r*temp));
+	return PV;
+}
 
-    int i;
+// CASE 2: PMT computation
+double PMTComputation(double PV, int n, double r)
+{
+	double PMT;
+	PMT = r * (1+ 1/(pow(1+r,n)-1)) * PV;
+	return PMT;
+}
+
+// CASE 3: n computation
+double NComputation(double PV, double PMT, double r)
+{
+	int n;
+	n = log(PMT/(PMT-r*PV))/log(1+r);
+	return n;
+}
+
+// CASE 4: interest rate computation with secant algorithm
+double Function(double PV, double PMT, int n, double r)
+{
+	double temp;
+	temp = r * (PV/PMT) * ( 1 + (1) / (pow(1+r,n) - 1) ) -1 ;
+	return temp;	
+}
+
+double InterestComputation(double PV, double PMT, int n) 
+{
+	double r = 0.001, rNext = 1, temp;
+	
+	while(abs(Function(PV, PMT, n, rNext)) > 0.000001)
+	{
+		temp = rNext;
+		rNext = rNext - (Function(PV, PMT, n, rNext)*(rNext-r))/(Function(PV,PMT,n,rNext)-Function(PV, PMT, n , r));
+		r = temp;
+	}
+
+	return rNext;
+}
+
+
+void MissingElementFI(int i = 0) 
+{
     double PMT, PV, n, r;
-    cout << "This program calculate the missing element for the Fixed Installment case." << endl;
-    cout << "In order to do so, which one of the four component you don't want to insert manually?" << endl;
-    cout
-            << " 1 = PV (Closing Balance) \n 2 = PMT (Fixed Installment) \n 3 = n (Number of Payment) \n 4 = r (Fixed Interest Rate)\n";
+    int MissingElement;
+    
+    if (i=0) 
+	{
+    	cout << "This program calculates the missing element for the Fixed Installment case." << endl;
+    	cout << "In order to do so, which one of the four component you don't want to insert manually?" << endl;	
+	} else
+	{
+		cout << "These are the possible inputs. Which one would you like not to insert?" << endl;
+	}
+	
+    cout << " 1 = PV (Opening balance) \n 2 = PMT (Fixed installment amount) \n 3 = n (Number of payments) \n 4 = r (Fixed interest rate)\n";
     cout << "Insert your choice by the provided numbers: \n";
-    cin >> MissingElementFI;
+    cin >> MissingElement;
 
-    if ( MissingElementFI == 1 ) {
-        cout << "You choose not to insert PV, Closing Balance.\n";
+    if ( MissingElement == 1 ) 
+	{
+        cout << "You choose not to insert PV, the opening balance.\n";
         cout << "Please insert PMT. \n";
         cin >> PMT;
         cout << "Please insert n.\n";
         cin >> n;
         cout << "Please insert r.\n";
         cin >> r;
-
-        double CalculatePV (long precision){
-
-            PV = (double) 1 / [PMT * [r * ( 1 + (double)(1 / (1+r)^n -1))]
-
-            return PV;
-        }
-    } else if ( MissingElementFI == 2 ) {
-        cout << "You choose not to insert PMT, Fixed Installment.\n";
+		
+		PV = PVComputation(PMT, n, r);
+		cout << "\nThe PV (opening balance) is: " << PV;
+		
+    } else if ( MissingElement == 2 ) 
+	{
+        cout << "You choose not to insert PMT, the fixed installment amount.\n";
         cout << "Please insert PV. \n";
         cin >> PV;
         cout << "Please insert n.\n";
         cin >> n;
         cout << "Please insert r.\n";
         cin >> r;
-
-        double CalculatePMT (long precision){
-
-            PMT = r*[1 + (double)(1 / (1+r)^n -1) ] * PV
-
-            return PMT;
-        }
-    } else if ( MissingElementFI == 3 ) {
-        cout << "You choose not to insert n, number of payment\n";
+		
+		PMT = PMTComputation(PV, n, r);
+		cout << "\nThe PMT (fixed installment amount) is: " << PMT;
+		
+    } else if ( MissingElement == 3 ) {
+        cout << "You choose not to insert n, the number of payments.\n";
         cout << "Please insert PV. \n";
         cin >> PV;
         cout << "Please insert PMT.\n";
         cin >> PMT;
         cout << "Please insert r.\n";
         cin >> r;
-
-        double Calculaten (long precision){
-
-            n = ((double)log(PMT / (PMT - r * PV) / log(1 + n)
-
-            return n;
-        }
-    } else if ( MissingElementFI == 4 ) {
-        cout << "You choose not to insert r, fixed interest rate \n";
+		
+		n = NComputation(PV, PMT, r);
+		cout << "\nThe n (number of payments) is: " << n;
+		
+    } else if ( MissingElement == 4 ) {
+        cout << "You choose not to insert r, the fixed interest rate.\n";
         cout << "Please insert PV. \n";
         cin >> PV;
         cout << "Please insert PMT.\n";
         cin >> PMT;
         cout << "Please insert n.\n";
         cin >> n;
+        
+        r = InterestComputation(PV, PMT, n);
+        cout << "\nThe r (fixed interest rate) is: " << r;
+        
     } else {
         cout << "Please insert a valid input.\n\n";
-        MissingElementFI (i = 1);
+        MissingElementFI(i = 1);
     }
 
-
 }
+
+
+#endif
