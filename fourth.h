4c203f74733ed61baec07b705cc3830b4a2e39f9
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-04-17 11:47:33 +0200

    Added all formulas

--- fourth.h
+++ fourth.h
@@ -1,11 +1,38 @@
-#ifndef fourth.h
-#define fourth.h
+#ifndef fourth_H
+#define fourth_H
 
 #include<iostream>
 #include<cmath>
 
 using namespace std;
 
+// CASE 1: PV computation
+double PVComputation(int n, double PMT, double r)
+{
+	double PV;
+	double temp;
+	temp = pow(1+r,n);
+	PV = PMT * ((temp-1)/(r*temp));
+	return PV;
+}
+
+// CASE 2: PMT computation
+double PMTComputation(double PV, int n, double r)
+{
+	double PMT;
+	PMT = r * (1+ 1/(pow(1+r,n)-1)) * PV;
+	return PMT;
+}
+
+// CASE 3: n computation
+double NComputation(double PV, double PMT, double r)
+{
+	int n;
+	n = log(PMT/(PMT-r*PV))/log(1+r);
+	return n;
+}
+
+// CASE 4: interest rate computation with secant algorithm
 double Function(double PV, double PMT, int n, double r)
 {
 	double temp;
@@ -22,11 +49,9 @@ double InterestComputation(double PV, double PMT, int n)
 	{
 		temp = rNext;
 		rNext = rNext - (Function(PV, PMT, n, rNext)*(rNext-r))/(Function(PV,PMT,n,rNext)-Function(PV, PMT, n , r));
-		cout << "r: " << r << " , temp: " << temp << " , rNext: " << rNext << " , Function is: " << Function(PV, PMT, n , rNext) << endl;
 		r = temp;
 	}
 
-	cout << "Interest is: " << rNext;
 	return rNext;
 
 }
