4f0f9063444981a5aebd6032f3ac37b6a6dfd173
HaasL <49677732+HaasL@users.noreply.github.com>
2019-04-18 13:54:54 +0200

    Update Dateincreasement.h

--- Dateincreasement.h
+++ Dateincreasement.h
@@ -1,1 +1,31 @@
+#ifndef Dateincreasement_H
+#define Dateincreasement_H
 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void Dateincreasement (int start[], int f){
+    if (f==1){
+        start[1]+=1;
+    } else if(f==2) {
+        start[0] += 6;
+        if (start[0] > 12) {
+            start[0] -= 12;
+            start[1]++;
+        }
+    }else if(f==3){
+        start[0]+=4;
+        if (start[0]>12){
+            start[0] -= 12;
+            start[1]++;
+        }
+    }else if(f==4){
+        start[0]++;
+        if (start[0]>12) {
+            start[0] -= 12;
+            start[1]++;
+        }}
+}
