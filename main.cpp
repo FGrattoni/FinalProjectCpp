4d4f71aeaf51b348e0cd5296d908c3955b703314
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-04-24 15:46:56 +0200

    Computation of the arrays with the data to print on the .cvs file

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 //#include <fstring>
 #include <cmath>
 
-#include "MissingElement.h"
+
+//TEMPORARY: used to display the arrays. (the function setw() )
+#include <iomanip>
+using std::setw;
+
+
+#include "MissingElementFI.h"
 #include "frequency.h"
+#include "output.h"
+#include "SaveVersion1.h"
+#include "SaveVersion2.h"
+#include "startingdate.h"
+#include "DateIncreasement.h"
 
 /*
 Final project: Loan Payment Schedule
@@ -16,9 +27,47 @@ using namespace std;
 
 int main()
 {
-    Frequency();
-    // need for ask wether Fixed installments
+    int f = Frequency();
+    
+    cout << "You would prefer a fixed installment or a fixed principal?" << endl;
+    cout << "Select by entering the number corresponding to your choice:" << endl;
+    cout << "[1] fixed installments;" << endl;
+    cout << "[2] fixed principal." << endl;
+    
+    cout << "\n.. temporary .. " << endl;
+	cout << "   There is no choice for the moment. The function is missing. \n\n";
+	
+	
+	// need for ask wether Fixed installments
     // if fixed installments
-    MissingElementFI();
+    
+	// FIXED INSTALLMENTs CASE
+	double* data = MissingElementFI(0, f);
+
+	int n = data[2];
+	double r = data[3];
+	double pmt = data[1];
+	
+	double pvb[n];
+	pvb[0] = data[0];
+	double p[n];
+	double interest[n];
+	double pve[n];
+	for(int i=0 ; i<n ; i++)
+	{
+		if (i!=0)
+		pvb[i] = pve[i-1];
+		interest[i] = roundf(pvb[i] * r * 100) / 100;
+		p[i] = pmt - interest[i];
+		pve[i] = pvb[i] - p[i];
+	}
+	
+
+
+	// THIS PART OF THE CODE IS JUST TO SEE THE TEMPORARY OUTPUT.
+	cout << "\npvb" << setw(13) << "p" << setw(13) << "Interest" << setw(13) << "pmt" << setw(13) << "pve"<<  endl;                     
+	for ( int i = 0; i<n; i++ )
+		cout << setw(7)<< pvb[i] << setw(13) << p[i] << setw(13) << interest[i] << setw(13) << pmt << setw(13) << pve[i] << endl;
+
     return 0;
 }
