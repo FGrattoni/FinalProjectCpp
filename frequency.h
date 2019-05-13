02cb52b5b75b8bfd8e6425624a8e8d2ae484d015
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-05-13 21:49:05 +0200

    Added input verification

--- frequency.h
+++ frequency.h
@@ -5,34 +5,31 @@
 #include<cmath>
 #include<string>
 
+#include "InputValidation.h"
+
 using namespace std;
 
-int Frequency(int i=0) {
-    int f;
-    if (i == 0){
-    cout << "Hello," << endl;
+int Frequency() {
     cout << "Considering your repayment schedule, how often would you like install repayment rates install payments per year?\n";
     cout << "Available rate options are:\n";
-    }
-    cout << " 1 = annually \n 2 = semi-annually \n 3 = quarterly \n 4 = monthly\n";
-    cout << "Insert your choice by the provided numbers: \n";
-    cin >> f;
+    cout << "[1] annually \n[2] semi-annually \n[3] quarterly \n[4] monthly\n";
+    cout << "Please, select by entering the number corresponding to your choice: ";
+    int f = ChoiceOption(1,4);
 
     if (f == 1) {
-    cout<<"You selected annual repayment rate\n";
+    cout<<"You selected annual repayment rate.\n";
     } else if(f == 2) {
-        cout<<"You selected semi-annual repayment rates\n";
+        cout<<"You selected semi-annual repayment rates.\n";
     } else if(f == 3) {
-        cout<<"You selected quarterly repayment rates\n";
+        cout<<"You selected quarterly repayment rates.\n";
         f = 4;
     } else if(f == 4) {
-        cout<<"You selected monthly repayment rates\n";
+        cout<<"You selected monthly repayment rates.\n";
         f = 12;
     } else {
-        cout<<"Please insert a valid input.\n\n";
-        Frequency(i = 1);
+        cout<<"ERROR. Something went wrong, please restart the program.";
     }
-    
+
     return f;
 }
 
