9e98f2e795f3436e37f74653b0f465ea22a101f6
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-05-16 10:48:04 +0200

    Simplified the program

--- DateIncreasement.h
+++ DateIncreasement.h
@@ -1,32 +1,17 @@
 #ifndef DateIncreasement_H
 #define DateIncreasement_H
 
-#include <iostream>
-#include <cmath>
-
 using namespace std;
 
-void DateIncreasement (int start[], int f){
-    if (f==1){
-        start[1]+=1;
-    } else if(f==2) {
-        start[0] += 6;
-        if (start[0] > 12) {
-            start[0] -= 12;
-            start[1]++;
-        }
-    }else if(f==3){
-        start[0]+=4;
-        if (start[0]>12){
-            start[0] -= 12;
-            start[1]++;
-        }
-    }else if(f==4){
-        start[0]++;
-        if (start[0]>12) {
-            start[0] -= 12;
-            start[1]++;
-        }}
+int* DateIncreasement(int start[], int f){
+
+    start[0] += (12/f);
+    if (start[0] > 12) {
+        start[0] -= 12;
+        start[1]++;
+    }
+
+    return start;
 }
 
 #endif
