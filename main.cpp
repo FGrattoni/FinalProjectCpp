e2ec1e5b2e8770a7b092a2202e57c253e20a8f75
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-05-16 10:47:26 +0200

    All output options implemented. Finished!

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using std::setw;
 #include "SaveVersion2.h"
 #include "startingdate.h"
 #include "DateIncreasement.h"
-#include "Floatingrate.h"
+#include "NormalVariable.h"
 
 /*
 Final project: Loan Payment Schedule
@@ -32,28 +32,25 @@ int main()
 
     int f = Frequency();
 
-    cout << "\nYou would prefer a fixed installment or a fixed principal?" << endl;
-    cout << "Please, select by entering the number corresponding to your choice:" << endl;
-    cout << "[1] fixed installments;" << endl;
-    cout << "[2] fixed principal." << endl;
+    //Choosing between the fixed installment and fixed principal case
+    int firstfix=0;
+    cout << "Which case would you like to consider?\n";
+    cout << "[1] fixed installments;\n";
+    cout << "[2] fixed principal.\n";
+    cout << "Select by entering the number corresponding to your choice: " << endl;
+    firstfix = ChoiceOption(1, 2);
 
-    cout << "\n.. temporary .. " << endl;
-	cout << "   There is no choice for the moment. The function is missing. \n\n";
-
-
-	// need for ask wether Fixed installments
-    // if fixed installments
-
-	// FIXED INSTALLMENTs CASE
+    if (firstfix == 1)
+    {   // FIXED INSTALLMENTs CASE
 	double* data = MissingElementFI(0, f);
 
 	int n = data[2];
 	double r = data[3];
 	double pmt[n];
 	pmt[0] = data[1];
-
 	double pvb[n];
 	pvb[0] = data[0];
+
 	double p[n];
 	double interest[n];
 	double pve[n];
@@ -73,76 +70,76 @@ int main()
 	pve[n-1] = pvb[n-1] - p[n-1];
 	pmt[n-1] = pvb[n-1] + interest[n-1];
 
-
-
 	// THIS PART OF THE CODE IS JUST TO SEE THE TEMPORARY OUTPUT.
 	cout << "\npvb" << setw(13) << "p" << setw(13) << "Interest" << setw(13) << "pmt" << setw(13) << "pve"<<  endl;
 	for ( int i = 0; i<n; i++ )
 		cout << setw(7)<< pvb[i] << setw(13) << p[i] << setw(13) << interest[i] << setw(13) << pmt[i] << setw(13) << pve[i] << endl;
 
-	int temp[2];
-	temp[0] = 01;
-	temp[1] = 2020;
-	SaveVersion1(temp, n, pvb, p, r, interest, pmt, pve, f);
+	SaveVersion1(start, n, pvb, p, r, interest, pmt, pve, f);
+    }
 
+    else {    //FIXED PRINCIPAL CASE
 
-/*
-     // FIXED PRINCIPAL CASE
-    double* data = Floatingrate(0, f);
-
-    double m=data[3];
-    int n=data[2];
-    double p=data[1];
-
-    //Creating Arrays for the Schedule two output
-    double pvb[n];
-    pvb[0] = data[0];
-    double p[n];
-    double interest[n];
-    double pve[n];
-    double pmt[n];
-    double m[n];
-    double b[n];
-    double pve[n];
     bool fixedinterest;
-
-
     cout << "You would prefer a fixed interest rate or a floating one?" << endl;
-    cout << "Select by entering the number corresponding to your choice:" << endl;
-    cout << "[0] floating one;" << endl;
+    cout << "[0] floating interest rate;" << endl;
     cout << "[1] fixed interest rate." << endl;
-    cin>>fixedinterest;
+    cout << "Select by entering the number corresponding to your choice: ";
+    fixedinterest = ChoiceOption(0, 1);
+
+    cout << "\nPlease, insert the amount of the loan: ";
+    double PV = RealInputLowerBound(0);
+    cout << "Please, enter the number of payments: ";
+    int n = ChoiceOption(1, 999);
+    double P = PV/n;
+    cout << "The fixed installment is of " << P << " euros." << endl;
+
+    double pvb[n];
+    pvb[0] = PV;
+    double interest[n];
+    double pmt[n];
+    double pve[n];
 
     if(fixedinterest){
-        cout<<"What should be the fixed interest rate? Please enter numbers in percentage (e.g. 1=1%\n"<<endl;
-        cin>>r;
-        r/=100;
-        cout<<"Your fixed interest rate is "<< r << endl;
+        cout<<"What should be the fixed interest rate? \nPlease enter number as percentage: ";
+        double r = RealInputBounded(0,100);
+        r /= 100;
+
+        double p[n];
 
         for(int i=0 ; i<n ; i++){
             if (i!=0)
                 pvb[i] = pve[i-1];
-            p[i]=data[1];
+            p[i] = P; // needed for the output of the file
             interest[i] = roundf(pvb[i] * r * 100) / 100;
-            pmt[i] = p[i] + interest[i];
-            pve[i] = pvb[i] - p[i];
+            pmt[i] = P + interest[i];
+            pve[i] = pvb[i] - P;
         }
 
+        SaveVersion1(start, n, pvb, p, r, interest, pmt, pve, f);
 
-        else{
-        if (for(int i=0 ; i<n ; i++)
+    }
+    else {  //FLOATING RATE CASE
+        cout << "Please, enter the fixed margin in percentage (suggested is 3 = 3%): ";
+        double m = RealInputBounded(0, 100);
+
+        double b[n];
+
+        for(int i=0 ; i<n ; i++)
             {
                 if (i!=0)
                     pvb[i] = pve[i-1];
-                p[i]=data[1];
-                b[i]=NormalVar(mean=3,std=0.5);
-                interest[i] = rounf(b[i]+m[i]) / 100;
-                pmt[i] =p[i] + interest[i];
-                pve[i] = pvb[i] - p[i];
-                }}
+                b[i] = NormalVar(3, 0.5);
+                interest[i] = roundf( (b[i]+m) * pvb[i] )  / 100;
+                pmt[i] = P + interest[i];
+                pve[i] = pvb[i] - P;
+            }
 
-*/
+        SaveVersion2(start, n, pvb, P, m, b, interest, pmt, pve, f);
+    }
 
+
+    }
 
         return 0;
-        }
+}
