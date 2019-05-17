5d583f053c806011106a4f5106d290b5ea659e95
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-05-17 21:49:06 +0200

    Minor changes

--- frequency.h
+++ frequency.h
@@ -10,21 +10,21 @@
 using namespace std;
 
 int Frequency() {
-    cout << "Considering your repayment schedule, how often would you like install repayment rates install payments per year?\n";
+    cout << "Considering your repayment schedule, how often per year would you like the installments?\n";
     cout << "Available rate options are:\n";
     cout << "[1] annually \n[2] semi-annually \n[3] quarterly \n[4] monthly\n";
     cout << "Please, select by entering the number corresponding to your choice: ";
     int f = ChoiceOption(1,4);
 
     if (f == 1) {
-    cout<<"You selected annual repayment rate.\n";
+        cout << "\nYou selected annual repayment rate.\n";
     } else if(f == 2) {
-        cout<<"You selected semi-annual repayment rates.\n";
+        cout << "\nYou selected semi-annual repayment rates.\n";
     } else if(f == 3) {
-        cout<<"You selected quarterly repayment rates.\n";
+        cout << "\nYou selected quarterly repayment rates.\n";
         f = 4;
     } else if(f == 4) {
-        cout<<"You selected monthly repayment rates.\n";
+        cout << "\nYou selected monthly repayment rates.\n";
         f = 12;
     } else {
         cout<<"ERROR. Something went wrong, please restart the program.";
