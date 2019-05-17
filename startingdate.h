d1c75b2792d1fc580d20f3417dbda321efe409d7
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-05-17 21:50:57 +0200

    Minor changes

--- startingdate.h
+++ startingdate.h
@@ -16,9 +16,9 @@ int * Start(int i = 0)
 
 	int* start = new int[2];
 	if (i == 0) cout << "When would you like to start your repayment schedule? \n";
-    cout<< "Please, enter the starting month[_ _]: ";
+    cout<< "Please, enter the starting month [1-12]: ";
     start[0] = ChoiceOption(1, 12);
-    cout<< "Please, enter the starting year[_ _ _ _]: ";
+    cout<< "Please, enter the starting year [current year-2039]: ";
     start[1] = ChoiceOption(currYear, 2039);
 
     if ( (start[0] < currMonth) && (start[1] == currYear) )
