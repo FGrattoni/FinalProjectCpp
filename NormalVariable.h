2ce2db62934fbaae18f95e7c81ca1ca51dedc5e3
FGrattoni <47087792+FGrattoni@users.noreply.github.com>
2019-05-17 23:18:03 +0200

    Fixed the creation of the normally distr. array

--- NormalVariable.h
+++ NormalVariable.h
@@ -8,21 +8,26 @@
 
 using namespace std;
 
-double NormalVar(double mean = 0, double std = 1)
+double * NormalVar(double mean = 0, double std = 1, int n = 1)
 {
 	double s, u, v, z1;
-	
+
+    double* b = new double[n];
+
 	srand((unsigned) time(0));
-	
-	do 	{
-		u = 2 * ( double(rand()) / RAND_MAX ) - 1;
-		v = 2 * ( double(rand()) / RAND_MAX ) - 1;
-		s = pow(u,2) + pow(v,2);
-	} while (s > 1);
-	
-	z1 = (u * sqrt( ( -2*log(s) ) / s )) * std + mean ;
-	
-	return z1;	
+
+	for (int i=0; i<n ; i++)
+    {
+        do 	{
+            u = 2 * ( double(rand()) / RAND_MAX ) - 1;
+            v = 2 * ( double(rand()) / RAND_MAX ) - 1;
+            s = pow(u,2) + pow(v,2);
+        } while (s > 1);
+
+        z1 = (u * sqrt( ( -2*log(s) ) / s )) * std + mean ;
+        b[i] = z1;
+    }
+	return b;
 }
 
 #endif
